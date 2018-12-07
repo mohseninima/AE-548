d4bc8a6d98eedf90e5984f0406d64512b295b0c0
mohseninima <mohseninima@yahoo.com>
2018-12-06 23:12:42 -0500

    Made the globe look cool

--- plot_track_3d.m
+++ plot_track_3d.m
@@ -16,11 +16,11 @@ function plot_track_3d(R,name,epoch,sys_cfg,sim_cfg)
     % Byproducts:
     %     Figure 31   Plot spacecraft 3D orbital track around the Earth
     %
-    % Author(s): Joseph Yates
+    % Author(s): Joseph Yates, Nima Mohseni
     % AEROSP 548 F18 Final Project: Ha, Mohseni, Yates
     %
     % Sources:
-    
+    %https://www.mathworks.com/matlabcentral/fileexchange/13823-3d-earth-example
     % Build reference sphere for Earth's surface
     [XE, YE, ZE] = sphere(30);
     
@@ -28,13 +28,21 @@ function plot_track_3d(R,name,epoch,sys_cfg,sim_cfg)
     figure(31)
     plot3(R(:,1),R(:,2),R(:,3),'-r');
     hold on;
-    surf(XE*sys_cfg.earth.R, YE*sys_cfg.earth.R, ZE*sys_cfg.earth.R);
+    image_file = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Land_ocean_ice_2048.jpg/1024px-Land_ocean_ice_2048.jpg';
+    globe = surf(XE*sys_cfg.earth.R, YE*sys_cfg.earth.R, -ZE*sys_cfg.earth.R);
+    cdata = imread(image_file);
+    set(globe, 'FaceColor', 'texturemap', 'CData', cdata, 'FaceAlpha', 1,'EdgeColor', 'none');
+    scatter3(-1e4 + 2e4*rand(500,1),1e4*ones(500,1),-1e4 + 2e4*rand(500,1),abs(1*randn(500,1)),'w','.')
+    scatter3(-1e4 + 2e4*rand(500,1),-1e4 + 2e4*rand(500,1),-1e4*ones(500,1),1*rand(500,1),'w','.')
+    scatter3(1e4*ones(500,1),-1e4 + 2e4*rand(500,1),-1e4 + 2e4*rand(500,1),1*rand(500,1),'w','.')
     hold off;
+    axis([-1e4,1e4,-1e4,1e4,-1e4,1e4])
     axis square
     title(['Orbital Trajectory of ' name ' since ' epoch], ...
         'fontsize',sim_cfg.plot.titleFontSize);
     xlabel('x_{ECI} [km]','fontSize',sim_cfg.plot.axisFontSize);
     ylabel('y_{ECI} [km]','fontSize',sim_cfg.plot.axisFontSize);
     zlabel('z_{ECI} [km]','fontSize',sim_cfg.plot.axisFontSize);
-    set(findall(gcf,'type','line'),'linewidth',2)
+    set(findall(gcf,'type','line'),'linewidth',1)
+    set(gca,'Color','k')
 end
